60e4f88793542077a104eac29e128ff2d6498b9f
Simcaa007 <havelkova.si@seznam.cz>
2026-02-25 17:42:35 +0100

    hotovo

--- 01/01.cpp
+++ 01/01.cpp
@@ -7,5 +7,30 @@ using namespace std;
 
 int main()
 {
-    
+    int n;
+    cout << "Zadejte cele cislo n pro n-prvkovou posloupnost: "; cin >> n;
+
+    int pole[100];
+    bool rostouci = true;
+
+    if (n > 100) {
+        cout << "Byl prekrocen limit pro velikost posloupnosti." << endl;
+    }
+    else {
+        cout << "A ted zadejte posloupnost prvku:" << endl;
+        for (int i = 0; i < n; i++) {
+            int cislo;
+            cin >> cislo;
+            pole[i] = cislo;
+            if (pole[i] < pole[i - 1]) rostouci = false;
+        }
+    }
+
+    if (rostouci) {
+        cout << "Ano, posloupnost prvku je rostouci" << endl;
+    }
+    else {
+        cout << "Tato posloupnost prvku NENI rostouci" << endl;
+    }
+
 }
