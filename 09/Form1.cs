c0783147c6f17635a1cf3ef81ef2a17160d6e2dc
Simcaa007 <havelkovas.07@spst.eu>
2026-02-27 08:51:46 +0100

    hotovo

--- 09/Form1.cs
+++ 09/Form1.cs
@@ -16,5 +16,66 @@ namespace _09
         {
             InitializeComponent();
         }
+
+        int n;
+        double[] pole;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            n = int.Parse(textBox1.Text);
+            pole = new double[n];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                double cislo = Math.Round(rnd.NextDouble() * (21 - 1) + 1, 2);
+                pole[i] = cislo;
+            }
+            Array.Sort(pole);
+            foreach (double d in pole)
+            {
+                listBox1.Items.Add(d);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            double b = double.Parse(textBox2.Text);
+            int pocet = 0;
+
+            foreach (double cislo in pole)
+            {
+                if (cislo <= b)
+                {
+                    pocet++;
+                }
+            }
+            double[] pole2 = pole.Take(pocet).ToArray();
+
+            foreach (double d in pole2)
+            {
+                listBox2.Items.Add(d);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            double b = double.Parse(textBox2.Text);
+
+            int pocet = 0;
+            foreach (double d in pole)
+            {
+                if (d <= b)
+                {
+                    pocet++;
+                }
+            }
+
+            double[] pole3 = pole.Skip(pocet).ToArray();
+
+            foreach(double d in pole3)
+            {
+                listBox3.Items.Add(d);
+            }
+        }
     }
 }
